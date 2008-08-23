8fbb7065c63e9141112c623f32d647fc5676d3ae
venkatesh88 <venkatesh88@ba4a88c6-b954-0410-a77a-f367d05c35db>
2008-08-23 22:56:37 +0000

    Basic Functionalities. Run As Eclipse Application, Create temporary project and temp.sw file, (contents being from resources-> temp.sw) and then Swid->PrintToConsole
    You will have to change the location of FileReader called in PrintPropertyListener to correct location of commonparsertestrdf.rdf in resources

---
+++ trunk/org.swid/src/org/swid/editors/SwidEditorMessages.java
@@ -0,0 +1,32 @@
+package org.swid.editors;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * @author venkatesh
+ *
+ */
+public class SwidEditorMessages {
+
+	public SwidEditorMessages() {
+	}
+	
+	private static final String RESOURCE_BUNDLE = "org.swid.editors.SwidMessages";//$NON-NLS-1$
+
+	private static ResourceBundle fgResourceBundle = ResourceBundle
+			.getBundle(RESOURCE_BUNDLE);
+
+	public static String getString(String key) {
+		try {
+			return fgResourceBundle.getString(key);
+		} catch (MissingResourceException e) {
+			return "!" + key + "!";//$NON-NLS-2$ //$NON-NLS-1$
+		}
+	}
+
+	public static ResourceBundle getResourceBundle() {
+		return fgResourceBundle;
+	}
+
+}
