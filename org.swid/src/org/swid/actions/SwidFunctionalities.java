c8f7e302342b907d32aa4eaf0b34f295f14b38a5
venkatesh88 <venkatesh88@ba4a88c6-b954-0410-a77a-f367d05c35db>
2008-08-31 19:59:59 +0000

    Some more basic stuff..to complete the basic print feature.

--- org.swid/src/org/swid/actions/SwidFunctionalities.java
+++ org.swid/src/org/swid/actions/SwidFunctionalities.java
@@ -1,7 +1,13 @@
 package org.swid.actions;
 
 import java.io.StringReader;
+import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.ui.PlatformUI;
+import org.ontoware.rdf2go.Reasoning;
 import org.swid.wikimodel.extensions.PrintPropertyListener;
 import org.wikimodel.wem.IWemListener;
 import org.wikimodel.wem.IWikiParser;
@@ -14,19 +20,48 @@ import org.wikimodel.wem.common.CommonWikiParser;
  * 
  */
 public class SwidFunctionalities {
-	
-	public static void parseAndPrint(String text) {
-		StringReader reader = new StringReader(text);
-		IWikiParser parser = new CommonWikiParser();
-		final StringBuffer buf = new StringBuffer();
-		IWemListener listener = new PrintPropertyListener(newPrinter(buf));
+
+	public static void parseAndPrint(final String text,final String dir,final String[] names,final String ns) {
 		try {
-			parser.parse(reader, listener);
-		} catch (WikiParserException e) {
-			e.printStackTrace();
+			PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
+				public void run(IProgressMonitor monitor)
+				throws InvocationTargetException, InterruptedException {
+					if(monitor==null)
+						monitor = new NullProgressMonitor();
+					try
+					{
+						monitor.beginTask("Working..", 100);
+						monitor.subTask("Reading..");
+						StringReader reader = new StringReader(text);
+						IWikiParser parser = new CommonWikiParser();
+						final StringBuffer buf = new StringBuffer();
+						String namespace = ns;
+						monitor.worked(10);
+						IWemListener listener = new PrintPropertyListener(newPrinter(buf), namespace, Reasoning.rdfs,dir,names);
+						try {
+							monitor.subTask("Parsing Input..");
+							parser.parse(reader, listener);
+							monitor.worked(70);
+						} catch (WikiParserException e) {
+							e.printStackTrace();
+						}
+						monitor.subTask("Printing...");
+						String test = buf.toString();
+						monitor.worked(10);
+						System.out.println(test);
+						monitor.worked(10);
+					}
+					finally
+					{
+						monitor.done();
+					}
+				}
+			});
+		} catch (InvocationTargetException e1) {
+			e1.printStackTrace();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
 		}
-		String test = buf.toString();
-		System.out.println(test);
 	}
 
 	protected static IWikiPrinter newPrinter(final StringBuffer buf) {
--- org.swid/src/org/swid/actions/SwidParsePrintToConsole.java
+++ org.swid/src/org/swid/actions/SwidParsePrintToConsole.java
@@ -1,10 +1,13 @@
 package org.swid.actions;
 
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.ui.texteditor.TextEditorAction;
 import org.swid.editors.SwidEditorMessages;
-import org.swid.actions.SwidFunctionalities;
 
 /**
  * @author venkatesh
@@ -22,14 +25,34 @@ public class SwidParsePrintToConsole extends TextEditorAction {
 		setText("PrintToConsole");
 		setEnabled(true);
 	}
-	
+
 	@Override
 	public void run()
 	{
 		ITextEditor editor = this.getTextEditor();
+		String ns = "http://code.google.com/p/wikimodel/resource/commonwikiparsertest#";
+		InputDialog dialog_ns = new InputDialog(this.getTextEditor().getEditorSite().getShell(),"Namespace","Enter Required namespace",ns,new IInputValidator() {
+			@Override
+			public String isValid(String newText) {
+				if(newText.startsWith("http://") && newText.endsWith("#"))
+					return null;
+				else
+					return "Should be of the form 'http://..../...#'";
+			} });
+		dialog_ns.create();
+		dialog_ns.open();
+		ns = dialog_ns.getValue();
 		IDocument doc = editor.getDocumentProvider().getDocument(editor.getEditorInput());
 		String text = doc.get();
-		SwidFunctionalities.parseAndPrint(text);
+		FileDialog dialog = new FileDialog(this.getTextEditor().getEditorSite().getShell(),SWT.MULTI);
+		//TODO Preferences..Set Reasoning option.
+		dialog.setFilterExtensions(new String[] {"*.rdf","*.owl"});
+		dialog.setText("Choose RDF file(s) to load..");
+		dialog.open();
+		String[] names = dialog.getFileNames();
+		String filterpath = dialog.getFilterPath()+"/";
+		//FIXME: The below method of sending the absolute path of directory is clumsy.. note debug does give /home/gen/.. in character array!
+		SwidFunctionalities.parseAndPrint(text,filterpath,names, ns);
 	}
 
 }
